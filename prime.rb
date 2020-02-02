f702dcd35fb3f14c5460848f8d0d892cd2f932b5
Willis Aybar <willisaybar@gmail.com>
2020-02-02 15:54:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,7 @@ require 'bigdecimal/math'
 puts Benchmark.measure { BigMath.PI(10_000) }
 
 def sieve(max)
+   primes = (0..max).to_a
 
 #define a method #prime?() that takes in an integer argument and returns a boolean true or false prime number
 
