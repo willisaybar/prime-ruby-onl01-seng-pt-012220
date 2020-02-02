352f55c966f8464a65e97def6dea4b8422c36a34
Willis Aybar <willisaybar@gmail.com>
2020-02-02 15:54:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 require 'benchmark'
 require 'bigdecimal/math'
 
+puts Benchmark.measure { BigMath.PI(10_000) }
+
 #define a method #prime?() that takes in an integer argument and returns a boolean true or false prime number
 
 def prime_lab(integer)
