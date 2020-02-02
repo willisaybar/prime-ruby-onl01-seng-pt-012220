0fca17233340e9c12a22a37ff74e77688fe07d26
Willis Aybar <willisaybar@gmail.com>
2020-02-02 15:55:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,6 +9,8 @@ def sieve(max)
    counter = 0
   primes.each do |p|
     next unless p
+    break if p*p > max
+    counter += 1
 
 #define a method #prime?() that takes in an integer argument and returns a boolean true or false prime number
 
