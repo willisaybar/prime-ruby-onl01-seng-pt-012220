ce0758c6844bcd715635a32ae02f601495421896
Willis Aybar <willisaybar@gmail.com>
2020-02-02 15:54:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,8 @@ require 'bigdecimal/math'
 
 puts Benchmark.measure { BigMath.PI(10_000) }
 
+def sieve(max)
+
 #define a method #prime?() that takes in an integer argument and returns a boolean true or false prime number
 
 def prime_lab(integer)
